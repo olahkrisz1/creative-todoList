99586875441499cd755230f64b91b87b98007e09
olahkrisz1 <olahkrisz1@gmail.com>
2023-05-24 22:12:51 +0200

    adding todos

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function addTodo(event) {
   todoDiv.classList.add("todo");
   // create LI
   const newTodo = document.createElement("li");
-  newTodo.innerText = "hey";
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
   // check mark button
@@ -26,8 +26,10 @@ function addTodo(event) {
   // check trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("complete-btn");
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   // append to list
   todoList.appendChild(todoDiv);
+  //clear todo input value
+  todoInput.value = "";
 }
